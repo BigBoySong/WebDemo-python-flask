2f600ec316efd75f465fb627464a0b938111f79a
BigBoySong <45398633+BigBoySong@users.noreply.github.com>
2018-11-28 01:16:42 +0800

    Update app.py

--- demo_sexphoto/app.py
+++ demo_sexphoto/app.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Date    : 2018-10-27
-# @Author  : ${author} (${email})
-# @Link    : ${link}
-# @Version : $Id$
-
-import os,random
+import os
 from db import *
 from flask import Flask ,render_template,jsonify,request
 
@@ -14,6 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
+
 @app.route('/get_item_list',methods=['POST'])
 def get_item_list():
 	if request.method=='POST':
@@ -27,11 +23,5 @@ def get_items_pics():
 		data=db_get_item_pic(item_id)
 		return jsonify(data)
 
-@app.route('/get_items_number',methods=['POST'])
-def get_items_number():
-	if request.method=='POST':
-		item_type=request.form['item_type']
-		return jsonify({"item_number":random.randint(20,100000000000)})
-
 if __name__=='__main__':
-	app.run(host='192.168.2.100',port='8090',debug=True)
+	app.run(host='localhost',port='80')
